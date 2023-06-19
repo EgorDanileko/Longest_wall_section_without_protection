d2b129782ca3ef9fbaf071add77dde79c541f0b9
egoro <egoro@DESKTOP-AQ8OOQN>
2023-06-19 13:08:17 +0200

    Add project files.

---
+++ Biro3/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+internal class Program {
+    public static void Main(string[] args) {
+
+        Console.WriteLine("Input amount of guarding posts:");
+        int N = Convert.ToInt32(Console.ReadLine());
+        while (N > 100 || N < 1)
+        {
+            Console.WriteLine("The input is wrong! Try again.");
+            N = Convert.ToInt32(Console.ReadLine());
+        }
+        int[] walls = new int[N];
+        int longestWall = 0;
+        int temporaryWall = 0;
+        longestWall = ReadData(walls, N, longestWall, temporaryWall);
+
+        OutRes(longestWall);
+        Console.WriteLine();
+    }
+    public static int ReadData(int[] walls, int N, int longestWall, int temporaryWall)
+    {
+        Console.WriteLine("Input the value for post 1:");
+        walls[0] = Convert.ToInt32(Console.ReadLine());
+        while (walls[0] < 0 || walls[0] > 1)
+        {
+            Console.WriteLine("The input is wrong! Try again.");
+            walls[0] = Convert.ToInt32(Console.ReadLine());
+        }
+
+
+
+        for (int i = 1; i < N; i++)
+        {
+            Console.WriteLine($"Input the valufe for post {i+1}");
+            walls[i] = Convert.ToInt32(Console.ReadLine());
+            while (walls[i] < 0 || walls[i] > 1)
+            {
+                Console.WriteLine("The input is wrong! Try again.");
+                walls[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            if (walls[i] + walls[i - 1] != 2)
+            {
+                temporaryWall++;
+            }
+            else
+            {
+                if (longestWall <= temporaryWall)
+                {
+                    longestWall = temporaryWall;
+                    temporaryWall = 0;
+                }
+                else
+                {
+                    temporaryWall = 0;
+                }
+            }
+        }
+        if (temporaryWall != 0 && longestWall == 0 || longestWall < temporaryWall)
+            longestWall = temporaryWall;
+
+        return longestWall;
+    }
+    public static void OutRes(int longest) 
+    {
+        Console.WriteLine("The longest wall section without protected walls:");
+        Console.WriteLine(longest);
+    }
+}
